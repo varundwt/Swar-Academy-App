c7093c15e7177ccb9470342612415855ce48f224
Varun Pandey <65996450+varundwt@users.noreply.github.com>
2024-08-01 10:33:28 +0530

    "next commit"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,9 +12,9 @@ export const metadata: Metadata = {
 
 export default function RootLayout({children,}: Readonly<{children: React.ReactNode;}>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body className={inter.className}>
-        <h2>Nav Item</h2>
+        <div className="relative w-full flex items-center justify-center"><Navbar/></div>        
         {children}</body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
-import Image from "next/image";
-import test from "node:test";
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
-  return (
-  <html lang="en">
-    <body>
-      <h1 className="text-2xl text-center mt-8">Hello Varun</h1>
-    </body>
-  </html>
+  return (  
+     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <HeroSection/>
+      </main>  
   );
 }
