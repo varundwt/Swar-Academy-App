0379643fb88cfda9f92992e32dd44d6849d9a76a
Varun Pandey <65996450+varundwt@users.noreply.github.com>
2024-08-02 17:55:22 +0530

    "Added Upcoming Webinars Component"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import FeaturedSection from "@/components/FeaturedSection";
 import HeroSection from "@/components/HeroSection";
 import MusicSchoolTestimonialCards from "@/components/MusicSchoolTestimonialCards";
 import WhyChooseUs from "@/components/ui/WhyChooseUs";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
 
 export default function Home() {
   return (  
@@ -10,6 +11,7 @@ export default function Home() {
       <FeaturedSection/>
       <WhyChooseUs/>
       <MusicSchoolTestimonialCards/>
+      <UpcomingWebinars/>
       </main>  
   );
 }
